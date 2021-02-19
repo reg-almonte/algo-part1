7f4a44763624d04e9bed85227985af99587dffa8
KM_V4_CI <jr@alink-group.co.jp>
2021-02-19 17:07:31 +0900

    For this exercise, the most important thing is the styel (90%)
    Issues encountered and fixed:
    
    1.
    % checkstyle *.java
    *-----------------------------------------------------------
    [WARN] HelloGoodbye.java:3:1: File contains tab characters (this is the first occurrence). Configure your editor to replace tabs with spaces. [FileTabCharacter]
    [WARN] HelloWorld.java:4:1: File contains tab characters (this is the first occurrence). Configure your editor to replace tabs with spaces. [FileTabCharacter]
    [WARN] RandomWord.java:7:1: File contains tab characters (this is the first occurrence). Configure your editor to replace tabs with spaces. [FileTabCharacter]
    [WARN] RandomWord.java:11:9: 'while' is not followed by whitespace. [WhitespaceAround]
    Checkstyle ends with 0 errors and 4 warnings.
    
    % custom checkstyle checks for HelloWorld.java
    *-----------------------------------------------------------
    
    % custom checkstyle checks for HelloGoodbye.java
    *-----------------------------------------------------------
    Checkstyle ends with 1 errors.
    [ERROR] HelloGoodbye.java:5:17: Do not use loops or conditionals in this program. [Design]
    Checkstyle ends with 1 error and 0 warnings.
    
    % custom checkstyle checks for RandomWord.java
    *-----------------------------------------------------------
    
    2.
    Testing correctness of HelloWorld
    *-----------------------------------------------------------
    Running 2 total tests.
    
    Test 1: check output format
      % java HelloWorld
      Hello, World!
    
      - student solution does not exactly match specified output format
    
    ==> FAILED
    
    Test 2: check correctness
      * java HelloWorld
        - reference output = 'Hello, World'
        - student   output = 'Hello, World!'
    ==> FAILED

--- week1/src/RandomWord.java
+++ week1/src/RandomWord.java
@@ -4,18 +4,17 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
 
-	public static void main(String[] args) {
-		int i = 0;
-		String output = "";
-		String inputWord = null;
-		while(!StdIn.isEmpty()) {
-			inputWord = StdIn.readString();
+    public static void main(String[] args) {
+        int i = 0;
+        String output = "";
+        String inputWord = null;
+        while (!StdIn.isEmpty()) {
+            inputWord = StdIn.readString();
             i++;
             if (StdRandom.bernoulli(1.0 / i)) {
                 output = inputWord;
             }
-		}
-        StdOut.println(output);		
-	}
-
+        }
+        StdOut.println(output);
+    }
 }
