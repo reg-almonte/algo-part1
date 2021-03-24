13c48f40c76e7a914a0c474efc43e55afa874683
reg-almonte <reg.almonte@gmail.com>
2021-03-24 17:55:23 +0900

    Refactored codes - Working/Tested codes

--- collinear/FastCollinearPoints.java
+++ collinear/FastCollinearPoints.java
@@ -9,6 +9,7 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class FastCollinearPoints {
@@ -17,54 +18,33 @@ public class FastCollinearPoints {
 
     // finds all line segments containing 4 or more points
     public FastCollinearPoints(Point[] points) {
-        count = 0;
-        int arraySize = points.length;
-        solution = new LineSegment[arraySize];
-
-        for (int i = 0; i < arraySize - 3; i++) {
-            sort(points, i); // Sort points[i+1] to points[arraySize-1]
-            double currentSlope = points[i].slopeTo(points[i]);
-            int counter = 0;
-            int largest = i;
-            int smallest = i;
-            for (int j = i + 1; j < arraySize - 2; j++) {
-                if (currentSlope == points[i].slopeTo(points[j])) {
-                    counter++;
-                    if (points[largest].compareTo(points[j]) < 0) {
-                        largest = j;
-                    }
-                    else if (points[smallest].compareTo(points[j]) > 0) {
-                        smallest = j;
-                    }
-                }
-                else {
-                    if (counter >= 4) {
-                        LineSegment newLineSegment = new LineSegment(points[smallest],
-                                                                     points[largest]);
-                        solution[count++] = newLineSegment;
-                    }
-                    counter = 1;
-                    largest = i;
-                    smallest = i;
-                    if (points[largest].compareTo(points[j]) < 0) {
-                        largest = j;
-                    }
-                    else if (points[smallest].compareTo(points[j]) > 0) {
-                        smallest = j;
-                    }
-                }
-
+        if (points == null) {
+            throw new IllegalArgumentException();
+        }
+        for (Point point : points) {
+            if (point == null) {
+                throw new IllegalArgumentException();
             }
         }
+
+        Arrays.sort(points);
+        for (int i = 0; i < points.length; i++) {
+            if (i > 0 && Double.compare(points[i].slopeTo(points[i - 1]),
+                                        Double.NEGATIVE_INFINITY) == 0) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        process(points);
     }
 
-    private static void merge(Point[] a, Point[] aux, int base, int lo, int mid, int hi) {
+    private static void merge(Point[] a, Point[] aux, Point base, int lo, int mid, int hi) {
         // assert isSorted(a, lo, mid); // precondition: a[lo..mid] sorted
         // assert isSorted(a, mid + 1, hi); // precondition: a[mid+1..hi] sorted
         for (int k = lo; k <= hi; k++)
             aux[k] = a[k];
         int i = lo, j = mid + 1;
-        Comparator<Point> slopeOrder = a[base].slopeOrder();
+        Comparator<Point> slopeOrder = base.slopeOrder();
         for (int k = lo; k <= hi; k++) {
             if (i > mid) a[k] = aux[j++];
             else if (j > hi) a[k] = aux[i++];
@@ -73,12 +53,51 @@ public class FastCollinearPoints {
         }
     }
 
-    public static void sort(Point[] a, int base) {
-        int N = a.length - base - 1;
+    private static void sort(Point[] a, Point base) {
+        int N = a.length;
         Point[] aux = new Point[N];
         for (int sz = 1; sz < N; sz = sz + sz)
-            for (int lo = base; lo < N - sz; lo += sz + sz)
-                merge(a, aux, base, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
+            for (int lo = 0; lo < N - sz; lo += sz + sz) {
+                merge(a, aux, base, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, a.length - 1));
+            }
+    }
+
+    private void process(Point[] points) {
+        count = 0;
+        int arraySize = points.length;
+        if (arraySize < 4) return; // Cannot form a 'line segment'
+        solution = new LineSegment[arraySize];
+        for (int i = 0; i < arraySize - 3; i++) {
+            Point[] copy = Arrays.copyOf(points, arraySize);
+            sort(copy, points[i]); // Sort points[i+1] to points[arraySize-1]
+            double currentSlope = points[i].slopeTo(points[i]);
+            int counter = 0;
+            int smallest = 0, largest = i;
+            for (int j = 0; j < arraySize; j++) {
+                if (currentSlope == points[i].slopeTo(copy[j])) {
+                    counter++;
+                    largest = j;
+                    if (copy[smallest].compareTo(copy[j]) > 0) smallest = j;
+                }
+                else {
+                    if (counter >= 3 && 0 == smallest) {
+                        LineSegment newLineSegment = new LineSegment(points[i],
+                                                                     copy[largest]);
+                        solution[count++] = newLineSegment;
+                    }
+                    currentSlope = points[i].slopeTo(copy[j]);
+                    counter = 1;
+                    largest = j;
+                    if (copy[0].compareTo(copy[j]) > 0) smallest = j;
+                    else smallest = 0;
+                }
+            }
+            if (counter >= 3 && 0 == smallest) {
+                LineSegment newLineSegment = new LineSegment(points[i],
+                                                             copy[largest]);
+                solution[count++] = newLineSegment;
+            }
+        }
     }
 
 
@@ -116,7 +135,11 @@ public class FastCollinearPoints {
         StdDraw.show();
 
         // print and draw the line segments
-        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        // print and draw the line segments
+        long start = System.currentTimeMillis();
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        long timeElapsed = System.currentTimeMillis() - start;
+        StdOut.println("Time elapsed: " + timeElapsed + " ms");
 
         StdOut.println("Count: " + collinear.numberOfSegments());
         for (LineSegment segment : collinear.segments()) {
